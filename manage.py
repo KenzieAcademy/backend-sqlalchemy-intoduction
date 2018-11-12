0241fba2815a008a57e38318b05eb2c2fb90f4e9
Bensch97 <ben.w.schacht@gmail.com>
2018-11-12 14:45:46 -0500

    Sprint A complete

--- manage.py
+++ manage.py
@@ -1,8 +1,47 @@
 import click
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
-from rdbms.models.database import Base
-from rdbms.models.database import engine
+Base = declarative_base()
 
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+
+class Posts(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    title = Column(String)
+    body = Column(String)
+
+class Comments(Base):
+    __tablename__ = 'comments'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    body = Column(String)
+
+class Tags(Base):
+    __tablename__ = 'tags'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    body = Column(String)
+
+engine = create_engine('sqlite:///mydatabase.db', echo=True)
+Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
+
+
+session = Session()
 
 @click.group()
 def cli():
@@ -12,7 +51,6 @@ def cli():
 @cli.command()
 def init():
     Base.metadata.create_all(engine)
-
 
 if __name__ == '__main__':
     cli()
