664bf935025d98897eef28d17a0d50b44a0f23ac
infosmith <dst.dsmith@gmail.com>
2018-11-12 16:30:30 -0500

    project command shortcuts

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+"""SQLAlchemy management commands."""
 import click
 
 from rdbms.models.database import Base
@@ -11,7 +12,24 @@ def cli():
 
 @cli.command()
 def init():
+    """Initialize database."""
     Base.metadata.create_all(engine)
+
+
+@cli.command()
+def seed():
+    """Seed database."""
+    import os
+    os.system("python {}".format('./rdbms/seeds_solution.py'))
+
+
+@cli.command()
+def wipe():
+    """Remove existing database, initialize and seed new database."""
+    import os
+    os.system("rm {}".format(os.getenv('DATABASE_URI').split('/')[-1]))
+    os.system("python manage.py init")
+    os.system("python manage.py seed")
 
 
 if __name__ == '__main__':
