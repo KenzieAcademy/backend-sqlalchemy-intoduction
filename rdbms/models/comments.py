9af5152db9d7083e5f74dd2d33e6e38e81c60e80
Jake Berg <jacobvictorberg@gmail.com>
2018-11-15 13:04:31 -0500

    Deleted excess files

--- rdbms/models/comments.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from .database import Base
-
-class Comments(Base):
-    __tablename__ = 'comment'
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(String, ForeignKey('user.id'))
-    body = Column(String)
