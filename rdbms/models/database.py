be3044b42f6b745981c1a080df82214caa404e73
Ashton Knoblock <Ashton.knoblock@gmail.com>
2018-11-19 10:55:48 -0500

    recommiting with generated db included in pr

--- rdbms/models/database.py
+++ rdbms/models/database.py
@@ -3,7 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///:memory:", echo = True)
+
+db_url = os.getenv('DB_URL', 'sqlite:///knob.db')
+
+engine = create_engine(db_url, echo = True)
 connection = engine.connect()
 
 Session = sessionmaker(bind=connection)
